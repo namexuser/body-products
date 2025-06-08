9239d8fe77f2577c881d7640fad9e88fcef0f57d
Eugene <eugeniusratshipa@gmail.com>
2025-06-08 13:01:13 +0200

    MIME fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // Explicitly set base path
   server: {
     host: "::",
     port: 8080,
